9ddd7420ca11c3727ba5da6ada5ae7e6a5392f14
Lisa Jung <lisahjung@gmail.com>
2020-03-20 18:46:39 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,61 +1,3 @@
-def addition (num1, num2)
-  num1+num2
-end
-
-def subtraction(num1, num2)
-  num1-num2
-end
-
-def multiplication(num1,num2)
-  num1*num2
-end
-
-def division (num1,num2)
-  num1/num2
-end
-
-def modulo (num1,num2)
-  num1%num2
-end
-
-def square_root (num)
-  Math.sqrt(num)
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
 def addition(num1, num2)
   num1+num2
 end
@@ -79,4 +21,3 @@ end
 def square_root(num)
   Math.sqrt(num)
 end
-=end
